7feee9f2d400b034e4c2143d4d0d0a0790365e95
justinG31 <gomezji@bc.edu>
2021-09-19 16:35:59 -0400

    Update main.go

--- main.go
+++ main.go
@@ -10,27 +10,17 @@ import (
 // used https://stackoverflow.com/questions/42213996/trying-to-parse-a-stdout-on-command-line-with-golang as a reference
 func main() {
 	// calls function to get user input
-<<<<<<< HEAD
-	var url, count = askInput()
-	sCount := strconv.Itoa(count)
-
-	for i := 0; i < 3; i++ {
-		go singlePing(sCount, url)
-	}
-
-	var input string
-	fmt.Scanln(&input)
-
-=======
 	var url1, url2, url3 = askInput()
 
 	fmt.Println("Pinging", url1)
-	singlePing(url1)
+	go singlePing(url1)
 	fmt.Println("Pinging", url2)
-	singlePing(url2)
+	go singlePing(url2)
 	fmt.Println("Pinging", url3)
-	singlePing(url3)
->>>>>>> fd45475f9743bade93ce52c4eabe181ae3151eeb
+	go singlePing(url3)
+
+	var input string
+	fmt.Scanln(&input)
 }
 
 // asks for user input of web address for ping and number of pings
