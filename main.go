7f07193c6c3ea3cd149120359e6f4ae643249789
justinG31 <gomezji@bc.edu>
2021-09-19 17:33:13 -0400

    Update main.go

--- main.go
+++ main.go
@@ -4,13 +4,45 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"sync"
 	"time"
 )
 
+<<<<<<< Updated upstream
 // used https://stackoverflow.com/questions/42213996/trying-to-parse-a-stdout-on-command-line-with-golang as a reference
 func main() {
 	// calls function to get user input
 	var url1, url2, url3 = askInput()
+=======
+//create variable to wait for go-routines to finish
+var waitToFinish sync.WaitGroup
+
+// data structure to capture ping data from go routines
+type pingData struct {
+	url  string
+	time time.Duration
+}
+
+// used https://stackoverflow.com/questions/42213996/trying-to-parse-a-stdout-on-command-line-with-golang as a reference
+func main() {
+	// calls function to get user input
+	var urlA, urlB, urlC = askInput()
+
+	// specifiy how many routines needed to be waited for
+	waitToFinish.Add(3)
+
+	// make channel and start go routines
+	c := make(chan pingData)
+	fmt.Println("Pinging", urlA)
+	go singlePing(urlA, c)
+	fmt.Println("Pinging", urlB)
+	go singlePing(urlB, c)
+	fmt.Println("Pinging", urlC)
+	go singlePing(urlC, c)
+
+	// receive data from channel
+	ping1, ping2, ping3 := <-c, <-c, <-c
+>>>>>>> Stashed changes
 
 	fmt.Println("Pinging", url1)
 	go singlePing(url1)
@@ -19,8 +51,9 @@ func main() {
 	fmt.Println("Pinging", url3)
 	go singlePing(url3)
 
-	var input string
-	fmt.Scanln(&input)
+	//let the go-routines finish running before the main function stops running
+	waitToFinish.Wait()
+	fmt.Println("All go-routines have finished")
 }
 
 // asks for user input of web address for ping and number of pings
@@ -56,5 +89,13 @@ func singlePing(url string) {
 	time2 := time.Now()
 	timeDiff := time2.Sub(time1)
 
+<<<<<<< Updated upstream
 	fmt.Println("runtime is", timeDiff)
+=======
+	// sends data through channel
+	c <- pingData{url, timeDiff}
+
+	// utilize the sync variable to know when the function is done running
+	defer waitToFinish.Done()
+>>>>>>> Stashed changes
 }
